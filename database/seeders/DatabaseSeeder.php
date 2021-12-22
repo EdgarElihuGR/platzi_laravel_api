8b1588b7c69cfa86ec45d80550df34f39c910b6d
Edgar Elihu Garcia Ramirez <elihu@netcommerce.mx>
2021-12-22 14:14:00 -0600

    Added comment model, migration, factory and controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         \App\Models\Post::factory(120)->create();
+        \App\Models\Comment::factory(160)->create();
     }
 }
